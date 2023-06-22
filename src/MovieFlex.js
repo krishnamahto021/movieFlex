aa1a939094e90dae87c19b9416f529122a447242
KRISHNA MAHATO <mkrishna98212@gmail.com>
2023-06-22 14:22:10 +0530

    set variable part of the component using state and update it using this.setState

--- src/MovieFlex.js
+++ src/MovieFlex.js
@@ -1,45 +1,102 @@
-import { Component } from 'react';
-import './movieFlex.css';
+import { Component } from "react";
+import "./movieFlex.css";
 class Movie extends Component {
-    render() {
-        return (
-            <>
-                <div className="movie-list">
-                    <div className="movie-container">
-                        <div className="left">
-                            <img src="https://m.media-amazon.com/images/I/91zzAMkVCUL._AC_UF1000,1000_QL80_.jpg" alt="movie-poster" className='poster' />
-                        </div>
-                        <div className="right">
-                            <div className="title">
-                                <span id="movie-title">The Avengers</span>
+  constructor(props) {
+    super(props);
+    this.state = {
+      movieTitle: "The avengers",
+      moviePlot:
+        "The remaining Avengers team up to reverse the devastating effects of Thanos snap and restore balance to the universe",
+      moviePrice: 99,
+      movieRating: 4.8,
+      movieStarCount: 4,
+    };
+  }
 
-                            </div>
-                            <div className="plot">
-                                <span id="movie-plot">The remaining Avengers team up to reverse the devastating effects of Thanos' snap and restore balance to the universe</span>
-
-                            </div>
-                            <div className="price"><span id="movie-price">
-                                Rs.99
-                            </span></div>
-
-                            <div className="footer">
-                                <div className="rating"><span id="movie-rating">4.8</span></div>
-                                <div className="starcount">
-                                    <img src="https://cdn-icons-png.flaticon.com/128/43/43625.png" alt="starminus" id='star-minus' />
-                                    <img src="
-                                https://cdn-icons-png.flaticon.com/128/1828/1828884.png" alt="star" id='star-img' />
-                                    <img src="https://cdn-icons-png.flaticon.com/128/748/748113.png" alt="starplus" id='star-plus' />
-                                </div>
-                                <div className="favourite"><button id='fav-btn'>Add to Favourites</button></div>
-                                <div className="buy"><button id='buy-btn'>Buy</button></div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </>
-        )
+  increaseStar = () =>{
+    if(this.state.movieStarCount >= 5){
+        return;
     }
+    this.setState((prevState)=>{
+        return{
+        movieStarCount : prevState.movieStarCount + 0.5
+        }
+    })
+  }
 
-};
+  decreaseStar = () => {
+    if(this.state.movieStarCount <= 0)
+    return;
+
+    this.setState((prevState)=>{
+        return{
+            movieStarCount:prevState.movieStarCount - 0.5
+        }
+    })
+  }
+
+
+
+
+  render() {
+    return (
+      <>
+        <div className="movie-list">
+          <div className="movie-container">
+            <div className="left">
+              <img
+                src="https://m.media-amazon.com/images/I/91zzAMkVCUL._AC_UF1000,1000_QL80_.jpg"
+                alt="movie-poster"
+                className="poster"
+              />
+            </div>
+            <div className="right">
+              <div className="title">
+                <span id="movie-title">{this.state.movieTitle}</span>
+              </div>
+              <div className="plot">
+                <span id="movie-plot">{this.state.moviePlot}</span>
+              </div>
+              <div className="price">
+                <span id="movie-price">Rs.{this.state.moviePrice}</span>
+              </div>
+
+              <div className="footer">
+                <div className="rating">
+                  <span id="movie-rating">{this.state.movieRating}</span>
+                </div>
+                <div className="starcount">
+                  <img
+                    src="https://cdn-icons-png.flaticon.com/128/43/43625.png"
+                    alt="starminus"
+                    id="star-minus" onClick={this.decreaseStar}
+                  />
+                  <img
+                    src="
+                                https://cdn-icons-png.flaticon.com/128/1828/1828884.png"
+                    alt="star"
+                    id="star-img"
+                  />
+                  <img
+                    src="https://cdn-icons-png.flaticon.com/128/748/748113.png"
+                    alt="starplus"
+                    id="star-plus" onClick={this.increaseStar}
+                  />
+                  <span id="star-count">{this.state.movieStarCount}</span>
+                </div>
+                <div className="favourite">
+                  <button id="fav-btn">Add to Favourites</button>
+                </div>
+                <div className="buy">
+                  <button id="buy-btn">Buy</button>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </>
+    );
+  }
+}
 
 export default Movie;
