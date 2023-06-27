d8df0328c7895c20ca531cdd4ffb55db2252d530
KRISHNA MAHATO <mkrishna98212@gmail.com>
2023-06-27 11:48:55 +0530

    create and add style to navbar using styled components

--- src/NavBar.js
+++ src/NavBar.js
@@ -1,10 +1,30 @@
-import './navBar.css'
+import styled from 'styled-components';
+
+const Nav = styled.div`
+height:70px;
+background-color: #1877f2;
+display:flex;
+justify-content:space-between;
+align-items:center
+`
+
+
+
 const NavBar = () => {
     return (
         <>
-            <div id="nav-bar">
-                <span id="logo-text">MovieFlex</span>
-            </div>
+            <Nav>
+                <span className='title'>MovieFlex</span>
+                <div className="cart" style={{marginBottom:'20px'}}>
+                    <div className="cart-image" >
+                        <img src="https://cdn-icons-png.flaticon.com/128/1170/1170678.png" alt="cart" style={{ height: '40px' }} />
+                    </div>
+
+                    <div className="cart-count" style={{ marginTop: '-50px' , marginLeft:'40px' }}>5</div>
+                </div>
+
+            </Nav>
+
         </>
     )
 }
