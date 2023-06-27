6dc6c2adb3ebe184ccc7374b748cea02504654fd
KRISHNA MAHATO <mkrishna98212@gmail.com>
2023-06-27 12:09:47 +0530

    style navbar using css module

--- src/NavBar.js
+++ src/NavBar.js
@@ -1,30 +1,18 @@
-import styled from 'styled-components';
-
-const Nav = styled.div`
-height:70px;
-background-color: #1877f2;
-display:flex;
-justify-content:space-between;
-align-items:center
-`
-
-
+import styles from './navBar.module.css';
 
 const NavBar = () => {
     return (
         <>
-            <Nav>
-                <span className='title'>MovieFlex</span>
-                <div className="cart" style={{marginBottom:'20px'}}>
-                    <div className="cart-image" >
-                        <img src="https://cdn-icons-png.flaticon.com/128/1170/1170678.png" alt="cart" style={{ height: '40px' }} />
+        <div className={styles.navBar}>
+                <span className={styles.title}>MovieFlex</span>
+
+                <div className={styles.cart} >
+                    <div >
+                        <img src="https://cdn-icons-png.flaticon.com/128/1170/1170678.png" alt="cart"  id={styles.cartImage} />
                     </div>
-
-                    <div className="cart-count" style={{ marginTop: '-50px' , marginLeft:'40px' }}>5</div>
+                    <div className={styles.cartCount}>5</div>
                 </div>
-
-            </Nav>
-
+        </div>
         </>
     )
 }
